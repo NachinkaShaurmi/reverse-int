8c40015006b35eea5c442abb279ad3ae3505b240
Yuriy <agent250691@yandex.ru>
2020-10-05 15:55:41 +0300

    feat: reverse-int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
+
+    let x = '';
+    (n.toString()[0] === '-') ? x = n.toString().slice(1) : x = n.toString();
+    return [...x].reverse().join('');
   
 }
